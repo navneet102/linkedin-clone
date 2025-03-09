21f0123697bcdd1906dbd0958d025b162f5ef69e
Navneet Kumar <conavneet@gmail.com>
2025-03-09 10:53:17 +0530

    Added basic controllers.

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js"; // for importing automatically just press ctrl+space on signup variable. Also dont forget to add .js in this line else it will crash.
 
 const router = express.Router();
 
