307b785fb2f257ac337a7e85b75371381cf2c8a9
Navneet Kumar <conavneet@gmail.com>
2025-03-09 10:30:15 +0530

    Added basic route.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.route.js"
+
 dotenv.config(); // configure dotenv to allow us to read from process.env
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use("/api/v1/auth", authRoutes);  // configure routes. /v1 is used for version control.
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
