0e4d8f9bcbedcf5409e8f3b14ab4e0a4de68d1e5
Navneet Kumar <conavneet@gmail.com>
2025-03-12 08:00:44 +0530

    Added logout and login controllers.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -65,10 +65,42 @@ export const signup = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
-    res.send("login");
-}
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        //Check if user exists
+        const user = await User.findOne({ username });
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        //Check password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        //Create and send tokens
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" }); //generates jwt token for a user.
+
+        await res.cookie("jwt-linkedin", token, {
+            httpOnly: true,  // prevent XSS cross site scripting don't allow javascript to access token
+            maxAge: 3 * 24 * 60 * 60 * 1000,
+            sameSite: "strict", //prevent CSRF attack
+            secure: process.env.NODE_ENV === "production", // HTTPS on production and HTTP on dev mode. Prevents man in the middle.
+        })
+
+        res.json({ message: "Logged in successfully" });
+
+    } catch (error) {
+        console.error("Error in login controller:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+};
 
 export const logout = (req, res) => {
-    res.send("logout");
+    res.clearCookie("jwt-linkedin");
+    res.json({ message: "Logged out succesfully" });
 }
