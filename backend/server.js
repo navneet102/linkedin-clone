5fd1b087b71f0918134f398f52ab7b39db3b2328
Navneet Kumar <conavneet@gmail.com>
2025-03-13 09:23:54 +0530

    Added notification routes/handlers.

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import postRoutes from "./routes/post.route.js"
+import notificationRoutes from "./routes/notification.route.js";
 
 import { connectDB } from "./lib/db.js"
 
@@ -17,7 +18,8 @@ app.use(cookieParser()); //helps in parsing cookies
 
 app.use("/api/v1/auth", authRoutes);  // configure routes. /v1 is used for version control.
 app.use("/api/v1/users", userRoutes);  
-app.use("/api/v1/posts", postRoutes);  
+app.use("/api/v1/posts", postRoutes);
+app.use("/api/v1/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
