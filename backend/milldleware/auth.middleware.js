63accd67378b2dbb2964c224dadad39ebd29d861
Navneet Kumar <conavneet@gmail.com>
2025-03-15 20:41:42 +0530

    Used backened and cookies to verify the login user.

--- backend/milldleware/auth.middleware.js
+++ backend/milldleware/auth.middleware.js
@@ -2,31 +2,29 @@ import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
-    try {
-        const token = req.cookies["jwt-likedin"]; //cookie parser in server.js is used for this.
+	try {
+		const token = req.cookies["jwt-linkedin"];
 
-        if (!token) {
-            return res.status(401).json({ message: "Unauthorized - No Token Provided" });
-        }
+		if (!token) {
+			return res.status(401).json({ message: "Unauthorized - No Token Provided" });
+		}
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		if (!decoded) {
+			return res.status(401).json({ message: "Unauthorized - Invalid Token" });
+		}
 
-        if (!decoded) {
-            return res.status(401).json({ message: "Unauthorized - Invalid Token" });
-        }
+		const user = await User.findById(decoded.userId).select("-password");
 
-        const user = await User.findById(decoded.userId).select("-password"); //This is the same userId in jwt token in signup controller. Select -password do not sends password as response, as it is not required.
+		if (!user) {
+			return res.status(401).json({ message: "User not found" });
+		}
 
-        if (!user) {
-            return res.status(401).json({ message: "User not found" });
-        }
+		req.user = user;
 
-        req.user = user; //It is important to do this as this means that user is verified and req.user is the same who is requesting any function. So we can skip protectRoute check if it is already present.
-
-        next();
-
-    } catch (error) {
-        console.log("Error in protectRoute: middleware", error.message);
-        return res.status(500).json({ message: "Internal server error" });
-    }
+		next();
+	} catch (error) {
+		console.log("Error in protectRoute middleware:", error.message);
+		res.status(500).json({ message: "Internal server error" });
+	}
 };
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,42 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from "./components/layout/Layout";
 import HomePage from './pages/HomePage';
 import SignUpPage from './pages/auth/SignUpPage';
 import LoginPage from './pages/auth/LoginPage';
-import { Toaster } from 'react-hot-toast';
+import { Toaster, toast } from 'react-hot-toast';
+import {useQuery} from "@tanstack/react-query";
+import { axiosInstance } from './lib/axios';
+
 
 function App() {
-  return <Layout>
 
+
+  const { data: authUser, isLoading } = useQuery({
+    queryKey: ["authUser"],  // This is used for getting this data elsewhere without calling this again and again
+    queryFn: async () => {
+      try {
+        const res = await axiosInstance.get("/auth/me");
+        return res.data;
+      } catch (err) {
+        if (err.response && err.response.status === 401) {
+          return null;
+        }
+        toast.error(err.response.data.message || "Something went wrong");
+      }
+    }
+  });
+
+  if(isLoading) return null; //To stop flickering effect when switching to pages by navigate method
+
+  return (<Layout>
     <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/signup' element={<SignUpPage />} />
-      <Route path='/login' element={<LoginPage />} />
+      <Route path='/' element={authUser ? <HomePage /> : <Navigate to={"/login"}/>} />
+      <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to={"/"}/>} />
+      <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to={"/"}/>} />
     </Routes>
     <Toaster />
   </Layout>
+  )
 }
 
 export default App;
