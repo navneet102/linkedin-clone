99cb6d8822b5e1e0e45c14003e3c71edb2ef471a
Navneet Kumar <conavneet@gmail.com>
2025-03-15 19:41:16 +0530

    made basic page setup and made signup page.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query' //Used for managing hooks easily
 
 const queryClient = new QueryClient()
+
+document.documentElement.setAttribute('data-theme', 'linkedin')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,3 @@
-import daisyui from 'daisyui';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,5 +7,22 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
-}
+  plugins: [require("daisyui")],
+  daisyui: {
+		themes: [
+			{
+				linkedin: {
+					primary: "#0A66C2", // LinkedIn Blue
+					secondary: "#FFFFFF", // White
+					accent: "#7FC15E", // LinkedIn Green (for accents)
+					neutral: "#000000", // Black (for text)
+					"base-100": "#F3F2EF", // Light Gray (background)
+					info: "#5E5E5E", // Dark Gray (for secondary text)
+					success: "#057642", // Dark Green (for success messages)
+					warning: "#F5C75D", // Yellow (for warnings)
+					error: "#CC1016", // Red (for errors)
+				},
+			},
+		],
+	},
+};
