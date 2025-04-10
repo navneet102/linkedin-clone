14477edf47c33b456a48f6621093f5d344487e87
Navneet Kumar <conavneet@gmail.com>
2025-04-10 09:31:32 +0530

    Setup for deployment.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
@@ -14,12 +15,15 @@ import { connectDB } from "./lib/db.js"
 dotenv.config(); // configure dotenv to allow us to read from process.env
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET","POST","PUT","DELETE"],
-}));
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+        methods: ["GET","POST","PUT","DELETE"],
+    }));
+}
 
 app.use(express.json({limit:"5mb"})); //parse JSON request bodies. //5mb limit for images or we get payloadTooLarge error.
 app.use(cookieParser()); //helps in parsing cookies
@@ -29,6 +33,14 @@ app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/notifications", notificationRoutes);
 app.use("/api/v1/connections", connectionRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
